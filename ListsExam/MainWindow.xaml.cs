edb846c4014e4c4df85766b7be496cd133a8f832
ZS10s105 <mateuszkowalski199700@gmail.com>
2022-12-15 08:16:19 +0100

    repair all stuf, again

---
+++ ListsExam/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListsExam
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<ToDoObject> ToDoList { get; set; }
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            exampleToDo();
+            DataContext = this;
+        }
+
+        public void exampleToDo()
+        {
+            ToDoList = new ObservableCollection<ToDoObject>();
+            ToDoList.Add(new ToDoObject("Wyprowadzenie psa", false, "do 4h"));
+            ToDoList.Add(new ToDoObject("Podlej kwiatki", true, "teraz"));
+            ToDoList.Add(new ToDoObject("Napisz kod", false, "jutro"));
+        }
+    }
+}
---
+++ ListsExam/ToDoObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListsExam
+{
+    public class ToDoObject
+    {
+        public string name { get; set; } = "";
+        public bool isDone { get; set; } = false;
+        public string date { get; set; } = "";
+
+        public ToDoObject(string name, bool isDone, string date)
+        {
+            this.name = name;
+            this.isDone = isDone;
+            this.date = date;
+        }
+    }
+}
